dfaf1b7d1bd6bfaf489f815fe38cb3d93ab13679
ahsanshah <shahahsan56@gmail.com>
2020-08-31 19:17:26 +0500

    Basic Question Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import QuestionCard from './components/QuestionCard';
+
 
 function App() {
+
+  const startQuiz=async()=>{
+
+  }
+
+  const checkAnswer=(e:React.MouseEvent<HTMLButtonElement>)=>{
+    
+  }
+
+  const nextQuestion=()=>{
+
+  }
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>React Quiz</h1>
+      <button className='startBtn' onClick={startQuiz}>Start Quiz</button>
+      <p className='score'>Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard  />
+      <button className='nextBtn' onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+type props={
+    question:string;
+    answers:string[];
+    callback:any;
+    userAnswer:boolean;
+    questionNumber:number;
+    totalQuestions:number;
+}
+
+const QuestionCard:React.FC<props> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNumber,
+    totalQuestions
+}) => {
+    return (
+        <div>
+            <p className="number">
+                Question:{questionNumber} / {totalQuestions}
+            </p>
+            <p dangerouslySetInnerHTML={{__html:question}} />
+            <div>
+                {answers.map(answer=>(
+                    <div>
+                        <button disabled={userAnswer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{__html:answer}} />
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default QuestionCard
